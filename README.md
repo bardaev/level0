7db816b58aafc649c022441e94d5e6dd0911ed12
Бадма Бардаев <null@MacBook-Air-Badma.local>
2022-07-21 21:00:44 +0300

    test and readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-go
+# level0
+
+> Структура проекта
+
+1. docker-compose.yml - поднимает базу данных и ants-streaming
+2. main.go - точка входа приложения
+3. model - модели
+4. service - содержит классы хранилища (БД и Кэш)
+5. handler - обработчики запросов
+6. пакет test - main.go для отправки данных в nats, mem_test.go юнит тест кеша
+
+> Запуск 
+
+1. docker-compose up
+2. go run main.go
+3. go run test/main.go
+
+> Использование
+
+Доступны 2 роута:
+
+1. localhost:8080/order/{id} - получить заказ по ид
+2. localhost:8080/orders - все заказы
---
+++ test/mem_test.go
@@ -0,0 +1,74 @@
+package test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"level0/model"
+	"level0/service"
+	"os"
+	"testing"
+)
+
+func TestMemStorageSave(t *testing.T) {
+	file, err := os.Open("model.json")
+	if err != nil {
+		t.Error("Cannot open file")
+	}
+
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		t.Error("Cant read data")
+	}
+
+	var wbOrder model.WbOrder
+	jsonErr := json.Unmarshal(data, &wbOrder)
+
+	if jsonErr != nil {
+		t.Error("Not valid json")
+	}
+
+	mem := service.MemStorageImpl{Data: make(map[uint]model.WbOrder)}
+	id, _ := mem.Save(&wbOrder)
+
+	got := mem.Data[id].ID
+	want := id
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestMemStorageGet(t *testing.T) {
+	file, err := os.Open("model.json")
+	if err != nil {
+		t.Error("Cannot open file")
+	}
+
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		t.Error("Cant read data")
+	}
+
+	var wbOrder model.WbOrder
+	jsonErr := json.Unmarshal(data, &wbOrder)
+
+	if jsonErr != nil {
+		t.Error("Not valid json")
+	}
+
+	mem := service.MemStorageImpl{Data: make(map[uint]model.WbOrder)}
+	id, _ := mem.Save(&wbOrder)
+
+	got := mem.Get(id)
+	want := id
+
+	if got.ID != want {
+		t.Errorf("got %q, want %q", got.ID, want)
+	}
+}
